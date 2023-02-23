fc9e94dd2c74f8236ced099a009666b334601273
Porson <porsonlee@gmail.com>
2023-02-23 14:58:26 -0800

    implemented axios to interact with the db server and refactored the functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import CreateItem from "./components/CreateItem";
 import ItemList from "./components/ItemList";
 
 function App() {
   const [items, setItems] = useState([]);
 
-  const editItemById = (id, newTitle) => {
+  // get all items from db
+  const fetchItems = async () => {
+    const response = await axios.get("http://localhost:3001/lists");
+    setItems(response.data);
+  };
+
+  // only render once when fetchItems is called.
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  // edit item
+  const editItemById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/lists/${id}`, {
+      title: newTitle,
+    });
+
     const updatedItems = items.map((item) => {
       if (item.id === id) {
-        return { ...item, title: newTitle };
+        return { ...item, ...response.data };
       }
       return item;
     });
@@ -16,7 +33,9 @@ function App() {
   };
 
   // delete an item off from the shopping list
-  const deleteItemById = (id) => {
+  const deleteItemById = async (id) => {
+    await axios.delete(`http://localhost:3001/lists/${id}`);
+
     const updatedItems = items.filter((item) => {
       return item.id !== id;
     });
@@ -24,11 +43,15 @@ function App() {
   };
 
   // add an item to the shopping list
-  const createItem = (item) => {
-    const randomID = Math.floor(Math.random() * 9999);
-    const updatedItems = [...items, { id: randomID, title: item }];
+  const createItem = async (item) => {
+    const response = await axios.post("http://localhost:3001/lists", {
+      title: item,
+    });
+
+    const updatedItems = [...items, response.data];
     setItems(updatedItems);
   };
+
   return (
     <div>
       <h1>Shopping List</h1>
